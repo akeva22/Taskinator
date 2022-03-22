0e5c32a4e1d53b98d10f113ffa0365f9de1b2eb8
akeva22 <melchorak@gmail.com>
2022-03-22 11:07:02 -0700

    Form reset and Input validator

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function () {
 
-    event.preventDefault();
+  event.preventDefault();
 
-    var taskNameInput = document.querySelector("input[name='task-name']").value
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-      // package up data as an object
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
@@ -24,8 +30,8 @@ var taskFormHandler = function () {
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj) {
-   
+var createTaskEl = function (taskDataObj) {
+
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
